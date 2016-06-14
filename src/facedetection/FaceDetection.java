6738f2b50ba9ac37e776314f2eb19a92a5fa6704
Jose Antonio Plata <platajose1@correo.ugr.es>
2016-06-14 20:15:26 +0200

    adding project

---
+++ src/facedetection/FaceDetection.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facedetection;
+import org.opencv.core.Core;
+/**
+ *
+ * @author plata
+ */
+public class FaceDetection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.loadLibrary("opencv_java310");
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME); // load native library of opencv
+
+        Ventana v = new Ventana();
+        v.setVisible(true);
+        v.setSize(600, 600);
+    }
+
+}
